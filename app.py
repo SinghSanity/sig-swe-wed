d57444262dee3631163ef379f996bee9038b29b2
Amandeep <asinghsanity@gmail.com>
2022-04-13 17:38:03 -0400

    worked on front end

--- app.py
+++ app.py
@@ -7,16 +7,14 @@ def hello():
     info = get_data()
     print(info)
 
-
-
-
     return render_template(
         'index.html', 
         song_name = info[0],
         artist_names = info[1],
         album_image = info[2],
         song_preview = info[3],
-        spotify_link = info[4]
+        spotify_link = info[4],
+        total_artists = len(info[1])
     )
 
 
