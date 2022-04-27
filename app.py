92fbd84f8f2fa0caeb186bf746952d830f36252d
Amandeep <asinghsanity@gmail.com>
2022-04-27 16:51:33 -0400

    finsihed backend functioanlity

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 '''This file is the main file for the application.'''
 from flask import Flask, render_template
 from data import get_data
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     '''This function is the main function for the application.'''
-    info = get_data()
+
+    '''Dua Lipa, Olivia Rodrigo, Ariana Grande'''
+    artist_list = ['6M2wZ9GZgrQXHCFfjv46we', '1McMsnEElThX1knmY4oliG', '66CXWjxzNUsdJxJ2JdwvnR']
+
+    rand = random.randint(0, len(artist_list) - 1)
+
+    info = get_data(artist_list[rand])
     print(info)
 
     return render_template(
--- data.py
+++ data.py
@@ -4,20 +4,19 @@ import requests
 from auth import get_auth
 
 
-def get_data():
+def get_data(artist_id):
     '''This function gets the data from the Spotify API.'''
     access_token = get_auth()
     headers = {
         'Authorization': 'Bearer {TOKEN}'.format(TOKEN=access_token)
     }
 
-    URL = 'https://api.spotify.com/v1/artists/{id}/top-tracks'.format(id='6M2wZ9GZgrQXHCFfjv46we')
+    URL = 'https://api.spotify.com/v1/artists/{id}/top-tracks'.format(id=artist_id)
     data = requests.get(URL + "?market=US", headers = headers)
 
     data = data.json()
 
-    #rand = random.randint(0, len(data['tracks']) - 1)
-    rand = 2
+    rand = random.randint(0, len(data['tracks']) - 1)
 
     artist_names = []
     for i in data["tracks"][rand]["artists"]:
